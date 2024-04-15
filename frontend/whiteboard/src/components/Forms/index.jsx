7ebf21ffadd039e51b75da9b214bbd55646909a8
NK4539 <nupurkapoor04@gmail.com>
2024-04-16 00:32:32 +0530

    forms

--- frontend/whiteboard/src/components/Forms/index.jsx
+++ frontend/whiteboard/src/components/Forms/index.jsx
@@ -1,5 +1,20 @@
+import CreateRoomForm from "./CreateRoomForm";
+import "./index.css";
+import JoinRoomForm from "./JoinRoomForm";
+
 const Forms=()=>{
-    return <h1>Forms working</h1>
+    return (
+        <div className="row">
+            <div className="col-md-4 mt-5 form-box p-5 rounded-2 mx-auto d-flex flex-column align-items-center ">
+                <h1 className="text">Create Room</h1>
+                <CreateRoomForm />
+            </div>
+            <div className="col-md-4 mt-5 form-box p-5 rounded-2 mx-auto d-flex flex-column align-items-center ">
+                <h1 className="text">Join Room</h1>
+                <JoinRoomForm />
+            </div>
+        </div>
+    );
 };
 
 export default Forms;
